606b00ae8b69f6bcd3830fe61c904ec378230f87
Namithalogishetty131 <124698814+Namithalogishetty131@users.noreply.github.com>
2023-04-10 00:38:18 -0400

    #change_language initial commit

--- change_language.py
+++ change_language.py
@@ -43,3 +43,20 @@ config = speech.RecognitionConfig(
     use_enhanced=True,
     model='phone_call'
 )
+
+
+async def change_language(ctx, language_code):
+    # Check if the language code is valid
+    if language_code not in LANGUAGES:
+        await ctx.send(f'Invalid language code. The supported languages are: {", ".join(LANGUAGES)}.')
+        return
+
+    # Update the language code in the Google Cloud Speech-to-Text configuration
+    config.language_code = language_code
+
+    # Send a confirmation message
+    await ctx.send(f'The language code has been updated to {LANGUAGES[language_code]}.')
+
+@bot.command(name='change-language')
+    async def change_language_command(ctx, language_code):
+        await change_language(ctx, language_code)
